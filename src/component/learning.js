b546d95b30ab13362ae01bacf1ecb3e5cf78eab8
Florin1103 <florinboboc11@gmail.com>
2023-10-06 19:33:39 +0300

    react dom

--- src/component/learning.js
+++ src/component/learning.js
@@ -12,14 +12,27 @@ class Learning extends Component{
       ]
     }
   }
+
+  clickHandler = () => {
+    // console.log("Hello!");
+    // alert("Welcome");
+    // this.state.fruits[0] = "samsung"
+    this.setState({
+      fruits:[
+        {name: "Samsung",weight: "120g"},
+        {name: "Apple",weight: "20g"},
+        {name: "Nokia",weight: "60g"},
+        ]
+    })
+   }
   render(){
-  
 
   return (
   <div>
+    <button onClick={this.clickHandler}>Click me!</button>
     <h1>Fruit name is {this.state.fruits[0].name}</h1>
     <h1>Fruit name is {this.state.fruits[1].name}</h1>
-  <h1>Fruit name is {this.state.fruits[2].name}</h1>
+    <h1>Fruit name is {this.state.fruits[2].name}</h1>
   </div>
   )
  }
